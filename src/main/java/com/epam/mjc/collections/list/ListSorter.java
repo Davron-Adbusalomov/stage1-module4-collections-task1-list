3d10775f744d6a6ba372c473c457d0bab39f152d
Davron-Adbusalomov <2110117@newuzbekistanuniversity.uz>
2024-02-08 11:40:05 +0500

    good

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,30 @@
 package com.epam.mjc.collections.list;
 
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        List<String> stringList = new LinkedList<>(sourceList);
+        stringList.sort(new ListComparator());
+        sourceList.clear();
+        sourceList.addAll(stringList);
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int absA = Math.abs(Integer.parseInt(a));
+        int absB = Math.abs(Integer.parseInt(b));
+
+        if (absA != absB) {
+            return Integer.compare(absA, absB);
+        } else {
+            // If absolute values are equal, compare the original values
+            return Integer.compare(Integer.parseInt(a), Integer.parseInt(b));
+        }
     }
 }
